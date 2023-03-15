87d951792777adca0565caa3f00c5842dd03b7b2
NIDHI SHARMA <nidhisharma07257@gmail.com>
2023-03-15 13:03:34 +0530

    Updates

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,6 @@
 resource "aws_s3_bucket" "s3" {
-  bucket = nidhi-bucket12
+  bucket = var.bucket_name
   tags = {
-    "Name" = "nidhi"
-    "Owner"= "nidhi.sharma@cloudeq.com"
-    "Purpose"="training"
+    "name" = "nidhi"
   }
 }
