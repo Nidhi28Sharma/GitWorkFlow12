0c6f49f56885c440b7b99dbd943a5e5ec3c36931
NIDHI SHARMA <nidhisharma07257@gmail.com>
2023-03-15 13:22:03 +0530

    commits

--- module/main.tf
+++ module/main.tf
@@ -1,5 +1,5 @@
 module "s3" {
   source = "../s3"
-  bucket = "h1gupta1-bucket"
-  tags = [ "hgupta","himanshu.gupta@cloudeq.com","traning" ]
+  bucket = "nidhi-buck"
+  tags = [ "nidhisharma","nidhi.sharma@cloudeq.com","traning" ]
 }
